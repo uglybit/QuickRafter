ab46031d38a9b67775e6d56b79b70f090918cf71
bluntrazor <adam.tepastrzala@wp.pl>
2021-02-23 22:03:31 +0100

    Add FixtureInterface
    All tests Fixtures will be inherit this

---
+++ Tests/FixtureInterface/FixtureInterface.h
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+
+
+class FixtureTest : public ::testing::Test {
+    protected:
+    std::istringstream input; 
+    std::ostringstream output; 
+    using cout_pointer = decltype(std::cout.rdbuf()); 
+    cout_pointer coutPtr; 
+
+    void SetUp() override{
+        
+        std::cin.rdbuf(input.rdbuf());
+        coutPtr = std::cout.rdbuf(); 
+        std::cout.rdbuf(output.rdbuf());  
+    }
+
+    void TearDown() override {
+        std::cout.rdbuf(coutPtr);
+    }
+};
+
